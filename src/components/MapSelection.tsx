dd409fbfa8b8011f98500b87f21dddfa5c33b79e
Pieczasz <bartekp854@gmail.com>
2024-11-20 20:59:51 +0100

    Marker changes

--- src/components/MapSelection.tsx
+++ src/components/MapSelection.tsx
@@ -24,17 +24,11 @@ interface MapEventsProps {
 }
 
 // Custom marker icon setup
-const customIcon: DivIcon = L.divIcon({
-  className: "custom-marker",
-  html: `
-    <svg viewBox="0 0 512 512" width="50" height="50">
-      <g transform="translate(0,512) scale(0.1,-0.1)" fill="#000000">
-        <path d="M2425 4890 c-579 -62 -1066 -441 -1260 -980 -65 -181 -77 -262 -82 -553 -5 -287 1 -362 42 -522 32 -123 84 -248 160 -385 138 -251 954 -1759 985 -1820 41 -82 113 -152 186 -180 156 -58 301 12 395 192 23 46 263 479 532 963 270 484 508 916 529 960 48 99 102 271 124 390 35 201 28 579 -16 773 -114 513 -487 926 -985 1091 -192 63 -426 91 -610 71z m298 -1046 c138 -31 291 -119 385 -222 104 -115 192 -303 192 -414 0 -99 -75 -272 -165 -381 -90 -108 -228 -199 -370 -243 -86 -27 -324 -27 -410 0 -142 44 -280 135 -370 243 -90 109 -165 282 -165 381 0 111 88 299 192 414 92 100 236 186 372 219 92 23 245 24 339 3z"/>
-      </g>
-    </svg>
-  `,
-  iconSize: [50, 50],
-  iconAnchor: [25, 50],
+const customIcon = L.icon({
+  iconUrl: "/locationMarker.svg",
+  iconSize: [24, 24],
+  iconAnchor: [16, 29],
+  popupAnchor: [5, -25],
 });
 
 // MapEvents component to handle click events
