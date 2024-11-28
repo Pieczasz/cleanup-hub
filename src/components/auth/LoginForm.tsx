0dcfba2ab2c5d0044029e426c3ae04fb90fb6a6c
Pieczasz <bartekp854@gmail.com>
2024-11-28 14:43:26 +0100

    Refactor LoginForm component: integrate CSRF token retrieval and simplify form handling

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -2,78 +2,30 @@
 
 // Functions
 import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
-import { useTransition } from "react";
-import { login } from "@/actions/login";
-import { useRouter } from "next/navigation";
-import { useSession } from "next-auth/react";
 
-// Zod
-import type * as z from "zod";
-import { LoginSchema } from "@/schemas";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { getProviders, getCsrfToken } from "next-auth/react";
 
 // Components
-import { Input } from "@/components/ui/input";
 import { CardWrapper } from "@/components/auth/CardWrapper";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormMessage,
-  FormItem,
-  FormLabel,
-} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import FormError from "@/components/FormError";
 import FormSuccess from "@/components/FormSuccess";
 
 const LoginForm = () => {
-  const [isPending, startTransition] = useTransition();
+  const [csrfToken, setCsrfToken] = useState("");
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
 
-  const router = useRouter();
-
-  const form = useForm<z.infer<typeof LoginSchema>>({
-    resolver: zodResolver(LoginSchema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
-
-  const onSubmit = async (values: z.infer<typeof LoginSchema>) => {
-    setError("");
-    setSuccess("");
-
-    startTransition(() => {
-      login(values)
-        .then((data) => {
-          if ("error" in data) {
-            setError(data.error);
-          } else if ("success" in data) {
-            setSuccess(data.success);
-            router.push("/");
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    });
-  };
-
-  const session = useSession();
-
   useEffect(() => {
-    if (session.status === "authenticated") {
-      router.push("/");
+    async function loadProviders() {
+      const csrf = await getCsrfToken();
+      setCsrfToken(csrf);
     }
-  }, [session.status, router]);
-
-  if (session.status === "authenticated") {
-    return null;
-  }
+    loadProviders().catch((error) => {
+      console.error("Failed to load providers:", error);
+    });
+  }, []);
 
   return (
     <>
@@ -84,57 +36,39 @@ const LoginForm = () => {
         cardFooterHrefText="Sign Up"
         showSocial
       >
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <form
+          className="space-y-6"
+          method="post"
+          action="/api/auth/callback/credentials"
+        >
+          <input type="hidden" name="csrfToken" value={csrfToken} />
+          <div>
             <div>
-              <FormField
-                control={form.control}
+              <label htmlFor="email">E-Mail</label>
+              <Input
                 name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>E-Mail</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="youremail@example.com"
-                        {...field}
-                        disabled={isPending}
-                        type="text"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem className="mt-4">
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="********"
-                        {...field}
-                        type="password"
-                        disabled={isPending}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
+                id="email"
+                placeholder="youremail@example.com"
+                type="text"
               />
             </div>
-            {/* Display errors */}
-            {error && <FormError message={error} />}
-            {success && <FormSuccess message={success} />}
-
-            <Button type="submit" className="w-full" disabled={isPending}>
-              Sign In
-            </Button>
-          </form>
-        </Form>
+            <div className="mt-4">
+              <label htmlFor="password">Password</label>
+              <Input
+                name="password"
+                id="password"
+                placeholder="********"
+                type="password"
+              />
+            </div>
+          </div>
+          {error && <FormError message={error} />}
+          {success && <FormSuccess message={success} />}
+          <Button type="submit" className="w-full">
+            Sign In
+          </Button>
+        </form>
       </CardWrapper>
-      <div className="flex items-center justify-center"></div>
     </>
   );
 };
