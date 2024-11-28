4477b4457655d1a6a71d446698d397ef43ed26f8
Pieczasz <bartekp854@gmail.com>
2024-11-28 16:05:32 +0100

    Refactor signIn and signUp pages to redirect authenticated users and improve session handling

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,12 +1,22 @@
+"use client";
 // Components
 import LoginForm from "@/components/auth/LoginForm";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-const page = () => {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <LoginForm />
-    </div>
-  );
+const Page = () => {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  if (session?.user.email) {
+    router.push("/");
+  } else {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <LoginForm />
+      </div>
+    );
+  }
 };
 
-export default page;
+export default Page;
--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -1,12 +1,22 @@
+"use client";
 // Components
 import RegisterForm from "@/components/auth/RegisterForm";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-const page = () => {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <RegisterForm />
-    </div>
-  );
+const Page = () => {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  if (session?.user.email) {
+    router.push("/");
+  } else {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <RegisterForm />
+      </div>
+    );
+  }
 };
 
-export default page;
+export default Page;
